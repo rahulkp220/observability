fec2f2e17db621e87cfeec25d6adad16052f67f3
Rahul Lakhanpal <rahulkp220@users.noreply.github.com>
2018-08-26 00:00:26 +0530

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -1,19 +1,19 @@
 module "observability" {
-    source = "../"
-    profile = "your_profile_here" ,
+    source              = "../"
+    profile             = "your_profile_here" ,
     aws_credential_path = "~/.aws/credentials",
-    aws_region = "your_aws_region", 
-    aws_ami = "your_aws_ami_here" ,
-    prometheus_count = 1 ,
-    alertmanager_count = 1 ,
-    grafana_count = 1 ,
-    pushgateway_count = 1 ,
-    aws_instance_type = "aws_instance_type_here" , 
-    key_name = "your_key_pair_here" ,
-    user = "ubuntu" ,
-    private_ssh_key = "ssh_key_path" ,
-    cidr_blocks = ["cider_range_here"] ,
-    outcidr_blocks = ["cider_range_here"] ,
-    vpc_id = "your_vpc_id_here" ,
-    has_public_ip = true
+    aws_region          = "your_aws_region", 
+    aws_ami             = "your_aws_ami_here" ,
+    prometheus_count    = 1 ,
+    alertmanager_count  = 1 ,
+    grafana_count       = 1 ,
+    pushgateway_count   = 1 ,
+    aws_instance_type   = "aws_instance_type_here" , 
+    key_name            = "your_key_pair_here" ,
+    user                = "ubuntu" ,
+    private_ssh_key     = "ssh_key_path" ,
+    cidr_blocks         = ["cidr_range_here"] ,
+    outcidr_blocks      = ["cidr_range_here"] ,
+    vpc_id              = "your_vpc_id_here" ,
+    has_public_ip       = true
 }
