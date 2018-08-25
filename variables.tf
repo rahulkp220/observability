6d40bab9555ac260584348095967c62b50c3c2da
Rahul Lakhanpal <rahulkp220@users.noreply.github.com>
2018-08-25 19:09:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,34 +1,20 @@
 variable "profile" {}
 variable "aws_credential_path" {}
-
 variable "aws_region" {}
-
 variable "aws_ami" {}
-
 variable "prometheus_count" {}
-
 variable "alertmanager_count" {}
-
 variable "grafana_count" {}
-
 variable "pushgateway_count" {}
-
 variable "aws_instance_type" {}
-
 variable "key_name" {}
-
 variable "user" {}
-
 variable "private_ssh_key" {}
-
 variable "cidr_blocks" {
   type    = "list"
-  }
-
+}
 variable "outcidr_blocks" {
   type    = "list"
 }
-
 variable "vpc_id" {}
-
 variable "has_public_ip" {}
