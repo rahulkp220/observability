4ae152718976aad2d239d694451ca9620a28e490
rahul lakhanpal <rahulkp220@gmail.com>
2018-08-25 18:29:29 +0530

    cleaner main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -3,17 +3,17 @@ module "observability" {
     profile = "your_profile_here" ,
     aws_credential_path = "~/.aws/credentials",
     aws_region = "your_aws_region", 
-    aws_ami = "ami-5e8bb23b" , # ubuntu 16.04 LTS
+    aws_ami = "your_aws_ami_here" ,
     prometheus_count = 1 ,
     alertmanager_count = 1 ,
     grafana_count = 1 ,
     pushgateway_count = 1 ,
-    aws_instance_type = "t2.micro" , 
+    aws_instance_type = "aws_instance_type_here" , 
     key_name = "your_key_pair_here" ,
     user = "ubuntu" ,
     private_ssh_key = "ssh_key_path" ,
-    cidr_blocks = ["0.0.0.0/0"] ,
-    outcidr_blocks = ["0.0.0.0/0"] ,
+    cidr_blocks = ["cider_range_here"] ,
+    outcidr_blocks = ["cider_range_here"] ,
     vpc_id = "your_vpc_id_here" ,
     has_public_ip = true
 }
